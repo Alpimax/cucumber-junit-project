373f4f62b0c71c09c03521416987a51ef4a40aa8
Alpimax <alperrozkann@gmail.com>
2022-10-25 08:52:52 -0400

    commit

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,4 +1,16 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
 }
