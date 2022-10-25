ddfaee54b10c93617d324469a8559ddacb67ed70
Alpimax <alperrozkann@gmail.com>
2022-10-25 11:23:08 -0400

    Hooks class created

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -6,11 +6,11 @@ import io.cucumber.java.Before;
 public class Hooks {
     @Before
     public void setup() {
-
+        System.out.println("This one before method that will run before method");
     }
 
     @After
     public void tearDown() {
-
+        System.out.println("This one after method that will run after method");
     }
 }
