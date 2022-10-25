3dbe3a82a8727b108ba8b484462b354d807a3eb5
Alpimax <alperrozkann@gmail.com>
2022-10-25 08:48:36 -0400

    couple modification created

--- src/test/java/com/cydeo/runner/CukerRunner.java
+++ src/test/java/com/cydeo/runner/CukerRunner.java
@@ -1,4 +1,18 @@
 package com.cydeo.runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/CukerRunner.html",
+        features ="src/test/resources/features/eating_cucumber.feature",
+        glue = "com/cydeo/step_definitions",
+        dryRun = true
+)
 public class CukerRunner {
+
+
 }
