6a734e81795c21f95cd60ddf3efef8151ddf94a3
Alpimax <alperrozkann@gmail.com>
2022-10-28 10:52:05 -0400

    Step added.

---
+++ src/test/java/com/cydeo/step_definitions/MyStepdefs.java
@@ -0,0 +1,14 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class MyStepdefs {
+    @Then("user should see below info in month dropdown")
+    public void userShouldSeeBelowInfoInMonthDropdown() {
+    }
+
+    @Given("user is on the dropdowns page of practice tool")
+    public void userIsOnTheDropdownsPageOfPracticeTool() {
+    }
+}
