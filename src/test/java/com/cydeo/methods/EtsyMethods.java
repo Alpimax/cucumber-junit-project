f474cd2ed93556a4d734e3c4402e1aafee39be7d
Alpimax <alperrozkann@gmail.com>
2022-11-02 23:46:58 -0400

    Hooks class created

--- src/test/java/com/cydeo/methods/EtsyMethods.java
+++ src/test/java/com/cydeo/methods/EtsyMethods.java
@@ -4,16 +4,15 @@ import com.cydeo.pages.Etsy;
 import com.cydeo.utilities.Driver;
 
 
+public class EtsyMethods extends Etsy {
 
-public class EtsyMethods extends Etsy  {
 
-
-    public void driver(){
+    public void driver() {
         Driver.getDriver().get("https://www.etsy.com/");
     }
 
     public void sendKey() {
-   searchArea.sendKeys("Woodspoon");
+        searchArea.sendKeys("Woodspoon");
     }
 
     public void searchButton() {
